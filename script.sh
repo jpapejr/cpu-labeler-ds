8bb59d9177b2692bb9ce6bcceb9735aafe451258
John Pape <jpapejr@icloud.com>
2021-02-10 15:13:17 -0500

    obsolete

--- script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-LABEL=`awk '/((^model name.*))/ { print $0 }' /proc/cpuinfo | cut -d'(' -f2 | cut -d')' -f1 | tr [:blank:]-, - | uniq`
-echo "kubectl label no/$NODE io.cpu.modelname=$LABEL --overwrite"
-kubectl label no/$NODE io.cpu.modelname=$LABEL --overwrite
-sleep 365d
